6b0715f3bf290273f987409bc9f3ae63e2cd6156
an <anthedev@gmail.com>
2023-08-04 07:35:56 -0700

    using components and maps hearts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import HeartLogo from './heart.svg';
+import Header from './components/Header';
+import Heart from './components/Heart';
+import HeartsList from './components/HeartsList';
 import './App.css';
-
-const message = 'cool cud';
-
-const Header = (props) => {
-  return <div className='App-header'>I ❤️ React</div>;
-}
-
-const Heart = (props) => {
-  return (
-    <div className='heart'>
-      <img className='heart-img' src={HeartLogo} alt='heart' />
-      <p className='heart-message'>{props.msg}</p>
-    </div>
-  )
-};
 
 function App() {
   return (
     <>
       <Header />
-      <Heart msg={message} />;
+      <HeartsList />;
     </>
   )
 }
